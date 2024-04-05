80750efb9b9681668bb1f8d222aa00db457f6bcc
Oliwier Kalecki <250969@edu.p.lodz.pl>
2024-04-05 16:24:00 +0200

    added div and fix text problems

--- main.cpp
+++ main.cpp
@@ -6,42 +6,57 @@ void usage(char *programName) {
     printf("Available operations:\n");
     printf("\tadd\n");
     printf("\tsub\n");
+    printf("\tdiv\n");
     printf("\tmul\n");
     printf("\n");
 }
 
 int main(int argc, char* argv[]) {
 
-    printf("Podano %d argumentow:\n", argc);
-    for (int i = 0; i < argc; i++) {
-        printf("Argument #%d\t-\t%s\n", i, argv[i]);
-        int number1 = 0;
-        int number2 = 0;
-        int result = 0;
+    int number1 = 0;
+    int number2 = 0;
+    int result = 0;
 
-        if (argc == 4) {
-            number1 = atoi(argv[1]);
-            number2 = atoi(argv[3]);
+    if (argc == 4) {
+        number1 = atoi(argv[1]);
+        number2 = atoi(argv[3]);
 
-            if (!strcmp("add", argv[2])) {
-                result = number1 + number2;
-                printf("%d + %d = %d\n", number1, number2, result);
-                return 0;
-            }
-            if (!strcmp("sub", argv[2])) {
-                result = number1 - number2;
-                printf("%d - %d = %d\n", number1, number2, result);
-                return 0;
-            }
-            if (!strcmp("mul", argv[2])) {
-                result = number1 * number2;
-                printf("%d * %d = %d\n", number1, number2, result);
-                return 0;
-            }
+        if (!strcmp("add", argv[2])) {
+            result = number1 + number2;
+            printf("%d + %d = %d\n", number1, number2, result);
+            return 0;
+        }
+        if (!strcmp("sub", argv[2])) {
+            result = number1 - number2;
+            printf("%d - %d = %d\n", number1, number2, result);
+            return 0;
         }
 
-        usage(argv[0]);
+        if (!strcmp("mul", argv[2])) {
+            result = number1 * number2;
+            printf("%d * %d = %d\n", number1, number2, result);
+            return 0;
+        }
 
-        return 0;
+        if (!strcmp("div", argv[2])) {
+            if (number2!= 0) {
+                result = number1 / number2;
+                printf("%d / %d = %d\n", number1, number2, result);
+            } else {
+                if (number1 == 0) {
+                    printf("%d / %d = NaN\n", number1, number2);
+                } else if (number1 > 0) {
+                    printf("%d / %d = +INF\n", number1, number2);
+                } else {
+                    printf("%d / %d = -INF\n", number1, number2);
+                }
+            }
+            return 0;
+        }
     }
+
+
+    usage(argv[0]);
+
+    return 0;
 }
